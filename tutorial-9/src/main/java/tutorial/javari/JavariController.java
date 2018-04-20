bd7b80b49ff9c9633a4d809541599f4457cac46a
Kristianto <liauwkristianto@gmail.com>
2018-04-20 16:48:46 +0700

    Added GET method with id parameter and DELETE method with id parameter

--- tutorial-9/src/main/java/tutorial/javari/JavariController.java
+++ tutorial-9/src/main/java/tutorial/javari/JavariController.java
@@ -1,32 +1,45 @@
 package tutorial.javari;
 
 import org.simpleflatmapper.csv.CsvParser;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tutorial.javari.animal.Animal;
-import tutorial.javari.animal.Condition;
-import tutorial.javari.animal.Gender;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class JavariController {
-    private static ArrayList<Animal> animalArrayList = new ArrayList<>();
+    private static ArrayList<Animal> animalList = new ArrayList<>();
     private File file;
 
     public JavariController() throws IOException {
         file = new File("./tutorial-9/src/main/java/tutorial/javari/animal_record.csv");
-        //CsvParser.forEach(file, row -> System.out.println(Arrays.toString(row)));
-        CsvParser.mapTo(Animal.class).forEach(file, animalArrayList::add);
+        CsvParser.mapTo(Animal.class).forEach(file, animalList::add);
     }
 
     @RequestMapping(value = "/javari", method = RequestMethod.GET)
-    public ArrayList<Animal> animals() {
-        return animalArrayList;
+    public ArrayList<Animal> getAllAnimals() {
+        return animalList;
     }
+
+
+    @RequestMapping(value = "/javari/{id}", method = RequestMethod.GET)
+    public List<Animal> getAnimalById(@PathVariable("id") int id){
+        Stream<Animal> getResult = animalList.stream().filter(animal -> animal.getId() == id);
+        return getResult.collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/javari/{id}", method = RequestMethod.DELETE)
+    public List<Animal> deleteAnimalById(@PathVariable("id") int id){
+        Stream<Animal> deleteResult = animalList.stream().filter(animal -> animal.getId() == id);
+        List<Animal> deletedAnimal = deleteResult.collect(Collectors.toList());
+        animalList.removeIf(animal -> animal.getId() == id);
+        return deletedAnimal;
+    }
+
 }
