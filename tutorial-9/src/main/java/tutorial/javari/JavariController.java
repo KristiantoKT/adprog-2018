c2610f1779abeafbf6897742a9d1fc3a137966b1
Kristianto <liauwkristianto@gmail.com>
2018-04-20 12:52:42 +0700

    Added CsvParser from other source

--- tutorial-9/src/main/java/tutorial/javari/JavariController.java
+++ tutorial-9/src/main/java/tutorial/javari/JavariController.java
@@ -1,5 +1,6 @@
 package tutorial.javari;
 
+import org.simpleflatmapper.csv.CsvParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,16 +9,23 @@ import tutorial.javari.animal.Animal;
 import tutorial.javari.animal.Condition;
 import tutorial.javari.animal.Gender;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 public class JavariController {
     private static ArrayList<Animal> animalArrayList = new ArrayList<>();
+    private File file;
+
+    public JavariController(){
+        file = new File("./tutorial-9/src/main/java/tutorial/javari/animal_record.csv");
+    }
 
     @RequestMapping(value = "/animal", method = RequestMethod.GET)
-    public ArrayList<Animal> animals(){
-        animalArrayList.add(new Animal(2,"Lion","nala",Gender.MALE,
-                150,130,Condition.HEALTHY));
+    public ArrayList<Animal> animals() throws IOException {
+        CsvParser.forEach(file, row -> System.out.println(Arrays.toString(row)));
         return animalArrayList;
     }
 }
